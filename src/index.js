48d3cc22bb4de15a42a3ad39768ce3d135ce4202
breitenbach <cesar.breitenbach@gmail.com>
2021-04-05 23:29:23 -0300

    curso de typescript configuração

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('demete');
+var teste = 'foda-se';
+var teste2 = 'fueda-se';
+console.log(teste2);
+console.log('kralho');
+console.log('kralho');
+console.log(teste);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('demete');
+const teste = 'foda-se';
+const teste2 = 'fueda-se';
+console.log(teste2);
+console.log('kralho');
+console.log('kralho');
+console.log(teste);
