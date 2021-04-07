ed21b93aafdddbaef8b8294e681219b4e8c32140
breitenbach <cesar.breitenbach@gmail.com>
2021-04-06 23:55:51 -0300

    exercicios do projeto euler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-console.log('demete');
-var teste = 'foda-se';
-var teste2 = 'fueda-se';
-console.log(teste2);
-console.log('kralho');
-console.log('kralho');
-console.log(teste);
+function fibo(x) {
+    if (x === 1)
+        return 1;
+    if (x === 2)
+        return 2;
+    return fibo(x - 1) + fibo(x - 2);
+}
+fibo(10);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
-console.log('demete');
-const teste = 'foda-se';
-const teste2 = 'fueda-se';
-console.log(teste2);
-console.log('kralho');
-console.log('kralho');
-console.log(teste);
+function fibo(x: number): number {
+  if (x === 1) return 1;
+  if (x === 2) return 2;
+  return fibo(x - 1) + fibo(x - 2);
+}
+
+function testaFibo(): number {
+  let indice = 0;
+  let valor = 0;
+  let soma = 0;
+
+  while (valor < 4000000) {
+    valor = fibo(indice + 1);
+    indice++;
+    if (valor % 2 === 0) {
+      soma += valor;
+    }
+  }
+
+  return soma;
+}
+
+const resposta = testaFibo();
+
+console.log(resposta);
