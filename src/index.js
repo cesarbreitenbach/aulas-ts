493a4622605959413f1f6e3c304cb9ac437e7d79
breitenbach <cesar.breitenbach@gmail.com>
2021-04-07 11:24:53 -0300

    remover js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-function fibo(x) {
-    if (x === 1)
-        return 1;
-    if (x === 2)
-        return 2;
-    return fibo(x - 1) + fibo(x - 2);
-}
-fibo(10);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 function fibo(x: number): number {
   if (x === 1) return 1;
   if (x === 2) return 2;
-  return fibo(x - 1) + fibo(x - 2);
+  else return fibo(x - 1) + fibo(x - 2);
 }
 
 function testaFibo(): number {
